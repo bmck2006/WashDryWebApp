371beb81614ec5b0cab48b1db93af285f219f81e
Brian McKeown <bmckeown2011@gmail.com>
2017-06-11 11:46:03 -0400

    Added JSON requests and began testing

--- main.py
+++ main.py
@@ -2,8 +2,24 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def index():
+
+    #TODO: activate the following block of code once rPi JSON object can be received
+    #reqeust and set json data from rPi
+    #wash1status = request.json['w1status']
+    #wash2status = request.json['w2status']
+    #dry1status = request.json['d1status']
+    #dry2status = request.json['d2status']
+
+    #Test wash/dry status on template
+    wash1status = 'available'
+    wash2status = 'available'
+    dry1status = 'available'
+    dry2status = 'available'
+
+    #conditional formatting for template found in index.html
+
     return render_template('index.html', index=index)
 
 if __name__ == "__main__":
